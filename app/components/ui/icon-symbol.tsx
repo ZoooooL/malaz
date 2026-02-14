0a8afb3933fc2a0b1c7b76f29b33e96ad823fe7e
Waleed mohamed <170679558+ZoooooL@users.noreply.github.com>
2026-02-14 16:52:57 +0300

    Delete app/components directory

--- app/components/ui/icon-symbol.tsx
+++
@@ -1,9 +0,0 @@
-import { Ionicons } from "@expo/vector-icons";
-import { ComponentProps } from "react";
-
-type IconProps = ComponentProps<typeof Ionicons>;
-
-export function IconSymbol({ name, size = 24, color, ...rest }: IconProps) {
-  return <Ionicons name={name} size={size} color={color} {...rest} />;
-}
-``
